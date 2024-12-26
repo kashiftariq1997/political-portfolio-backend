5bba893627ef09d4fd74061cf797139f18cac145
kashiftariq1997 <kashiftariq848@gmail.com>
2024-12-26 17:58:10 +0500

    add db url in env

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (!fs.existsSync(tempDir)) {
 }
 
 // Database Connection
-mongoose.connect('mongodb://localhost:27017/node_mongo_app', {
+mongoose.connect( process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB')).catch(err => console.error(err));
